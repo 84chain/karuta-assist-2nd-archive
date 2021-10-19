4578edb757e670dba094ab349764be7c88a06b69
84chain <84chain@users.noreply.github.com>
2021-10-19 16:10:25 -0700

    A

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ def rankUsers(load):
     for i in net_correct:
         ratios.append({
             "Visitor": i[0],
-            "Ratio": i[1]/i[-1] * ((max(len([j for j in load if str(j["Visitor"]) == str(i[0])]), 10) - 10)/len(load))
+            "Ratio": (i[1]/i[-1] if i[-1] != 0 else 0) * ((max(len([j for j in load if str(j["Visitor"]) == str(i[0])]), 10) - 10)/len(load))
         })
     return ratios
 
@@ -459,7 +459,7 @@ async def datestats(ctx, *args):
     net_correct = sum([i["Result"] for i in userinfo])
     total_answers = len(userinfo)
 
-    ratio = net_correct/correct_answers * ((max(total_answers, 10) - 10)/len(load))
+    ratio = (net_correct/correct_answers if correct_answers != 0 else 0) * ((max(total_answers, 10) - 10)/len(load))
 
     ranks = sorted(rankUsers(load), key=lambda x: x["Ratio"], reverse=True)
     rank = ranks.index({
@@ -473,7 +473,7 @@ async def datestats(ctx, *args):
     stats.add_field(name="Net Correct", value=f"{net_correct} -> [`{correct_answers}` + `0 × {neutral_answers}` - `{wrong_answers}`] out of {total_answers} ({Round(ratio * 100)}%)", inline=False)
     stats.add_field(name="Rank", value=f"{rank} out of {len(ranks) + 1}", inline=False)
     stats.set_thumbnail(url=botIcon)
-    stats.set_footer(text="Rank is calculated by Net Correct / Total Answers")
+    stats.set_footer(text="Rank is calculated by Net Correct / Total Correct × (max(total_answers, 10) - 10) / All Answers")
     await trymsg.delete()
     await ctx.send(embed=stats)
 
